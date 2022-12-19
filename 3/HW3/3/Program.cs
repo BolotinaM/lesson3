7ae16da2d71252306c9c9106715260fc15ddef94
Mariia <bolotina.23reg@yandex.ru>
2022-12-19 18:39:24 +0300

    Добавили решение задачи №3 (оформили табличкой как в лекционном маретиале урока № 4) в доп.ветке

---
+++ 3/HW3/3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27 - пример
+// 5 -> 1, 8, 27, 64, 125 - пример
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    for (int j = 1; j < 6; j++)
+    {
+        Console.Write($"{i*i*i} ");
+        i = i+1;
+    }
+    Console.WriteLine("");
+}
+
+// Функции задачи № 3
+    // Вызов: строка № 5
+    int ReadInt(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
