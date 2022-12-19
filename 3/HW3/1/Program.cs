435bd24d2ca7e311b785ddf4c3a15d29d6ac66d7
Mariia <bolotina.23reg@yandex.ru>
2022-12-19 18:23:32 +0300

    Добавили решение задачи №2 в доп.ветке

---
+++ 3/HW3/1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("ВВОД");
+var number = Console.ReadLine(); 
+int len = number.Length;
+
+    if (len == 5)
+    {
+        if (number[0] == number[4] && number[1] == number[3])
+        {
+            Console.WriteLine("истина");
+        }
+        else
+        {
+            Console.WriteLine("ложь");
+        }
+    }
+
+    else
+    {
+        Console.WriteLine("ошибка");
+    }
